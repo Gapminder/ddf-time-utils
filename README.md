6c42aee3a0a8428650d846cf5b9cfdeb557830f9
buchslava <vyacheslav.chub@gmail.com>
2016-08-01 18:12:03 +0300

    documentation

--- README.md
+++ README.md
@@ -1,6 +1,125 @@
-# Gapminder DDF Time Tools
-> This is the gapminder DDF time tools library
+# Gapminder DDF Time Utils
 
-### How to install
+## Install
 
-`npm i ddf-time-tool`
+`npm i ddf-time-utils`
+
+## Description
+
+`ddf-time-utils` is created for DDF time format checking and manipulation.
+
+## Exported constants
+
+ * YEAR_TYPE
+ * QUARTER_TYPE
+ * MONTH_TYPE
+ * WEEK_TYPE
+ * DATE_TYPE
+
+These constants describes five different kinds of time supported by DDF.
+
+## Exported functions
+
+ * getTimeRange(timeRangeQuery): timeRange
+ * detectTimeType(time): timeType
+ 
+More info - in `Examples` section.
+
+## Examples
+
+### Time type detection
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+
+const resultYear = ddfTimeUtils.detectTimeType('2000');
+const resultQuarter = ddfTimeUtils.detectTimeType('2000q1');
+const resultMonth = ddfTimeUtils.detectTimeType('200001');
+const resultWeek = ddfTimeUtils.detectTimeType('2000w01');
+const resultDate = ddfTimeUtils.detectTimeType('2000102');
+
+console.log(resultYear === ddfTimeUtils.YEAR_TYPE);
+console.log(resultQuarter === ddfTimeUtils.QUARTER_TYPE);
+console.log(resultMonth === ddfTimeUtils.MONTH_TYPE);
+console.log(resultWeek === ddfTimeUtils.WEEK_TYPE);
+console.log(resultDate === ddfTimeUtils.DATE_TYPE);
+```
+
+### Time type detection (incorrect time)
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+const result = ddfTimeUtils.detectTimeType('200x0102');
+
+console.log(result === null);
+```
+
+### Time range detection
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+
+const simpleResult = ddfTimeUtils.getTimeRange(['1990']);
+const multipleResult = ddfTimeUtils.getTimeRange(['1800', ['2013', '2015'], '2017', '980']);
+
+console.log(simpleResult);
+console.log(multipleResult);
+```
+
+Result:
+```
+[ '1990' ]
+[ '1800', '2013', '2014', '2015', '2017', '980' ]
+```
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+
+const result = ddfTimeUtils.getTimeRange([['1990q1', '1992q2'], '1993q4']);
+
+console.log(result);
+```
+
+Result:
+```
+[ '1990q1',
+  '1990q2',
+  '1990q3',
+  '1990q4',
+  '1991q1',
+  '1991q2',
+  '1991q3',
+  '1991q4',
+  '1992q1',
+  '1992q2',
+  '1993q4' ]
+```
+
+### Time range detection (incorrect way)
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+// time range detection is possible only for same kind of time
+// we have `year` and `quarter` types in same query
+const result = ddfTimeUtils.getTimeRange(['2015', '2011q1']);
+
+console.log(result);
+```
+
+Result:
+```
+null
+```
+
+```
+const ddfTimeUtils = require('ddf-time-utils');
+// unexpected time
+const result = ddfTimeUtils.getTimeRange(['foo']);
+
+console.log(result);
+```
+
+Result:
+```
+null
+```
--- test/main.spec.js
+++ test/main.spec.js
@@ -10,18 +10,18 @@ const incorrectQueries = require('./fixtures/incorrect-queries');
 chai.use(sinonChai);
 
 describe('DDF time utils', () => {
-  describe('when correct sets of data (in fixtures/good-cases)', () => {
+  describe('when correct sets of time ranges (in fixtures/good-cases)', () => {
     Object.getOwnPropertySymbols(goodCasesFixtures).forEach(timeType => {
       describe(`for '${Symbol.keyFor(timeType)}' type`, () => {
-        it(`result for simple
-        query '${JSON.stringify(goodCasesFixtures[timeType].simple)}' should be expected`, () => {
+        it(`should time range for simple
+        query '${JSON.stringify(goodCasesFixtures[timeType].simple)}' be expected`, () => {
           const simpleResult = ddfTimeUtils.getTimeRange(goodCasesFixtures[timeType].simple);
 
           expect(_.isEqual(simpleResult, goodCasesFixtures[timeType].simple)).to.be.true;
         });
 
-        it(`result for multiple
-        query '${JSON.stringify(goodCasesFixtures[timeType].multiple.query)}' should be expected`, () => {
+        it(`should time range for multiple
+        query '${JSON.stringify(goodCasesFixtures[timeType].multiple.query)}' be expected`, () => {
           const multipleResult = ddfTimeUtils.getTimeRange(goodCasesFixtures[timeType].multiple.query);
 
           expect(_.isEqual(multipleResult, goodCasesFixtures[timeType].multiple.result)).to.be.true;
@@ -30,17 +30,55 @@ describe('DDF time utils', () => {
     });
   });
 
-  describe('when incorrect query', () => {
+  describe('when incorrect time ranges query', () => {
     Object.getOwnPropertySymbols(incorrectQueries.queries).forEach(queryType => {
       describe(`and type is '${Symbol.keyFor(queryType)}'`, () => {
         const errorDescription = 'Wrong time query format';
 
-        it(`error '${errorDescription}' should be thrown`, () => {
+        it(`should error '${errorDescription}' be thrown`, () => {
           const result = ddfTimeUtils.getTimeRange(incorrectQueries.queries[queryType]);
 
           expect(result).to.be.null;
         });
       });
+    });
+  });
+
+  describe('when detect time type', () => {
+    it('should year detection be correct', () => {
+      const result = ddfTimeUtils.detectTimeType('2000');
+
+      expect(result).to.equal(ddfTimeUtils.YEAR_TYPE);
+    });
+
+    it('should quarter detection be correct', () => {
+      const result = ddfTimeUtils.detectTimeType('2000q1');
+
+      expect(result).to.equal(ddfTimeUtils.QUARTER_TYPE);
+    });
+
+    it('should month detection be correct', () => {
+      const result = ddfTimeUtils.detectTimeType('200001');
+
+      expect(result).to.equal(ddfTimeUtils.MONTH_TYPE);
+    });
+
+    it('should week detection be correct', () => {
+      const result = ddfTimeUtils.detectTimeType('2000w01');
+
+      expect(result).to.equal(ddfTimeUtils.WEEK_TYPE);
+    });
+
+    it('should date detection be correct', () => {
+      const result = ddfTimeUtils.detectTimeType('2000102');
+
+      expect(result).to.equal(ddfTimeUtils.DATE_TYPE);
+    });
+
+    it('should incorrect time detection be null', () => {
+      const result = ddfTimeUtils.detectTimeType('200x0102');
+
+      expect(result).to.equal(null);
     });
   });
 });
